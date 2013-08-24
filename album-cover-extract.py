4411c73e728e4775bf6676b7947db6523dfc3f6c
Barosl LEE <vcs@barosl.com>
2013-08-24 14:39:10 +0900

    album-cover-extract.py: Add support for MPEG-4 Audio (or AAC) files

--- album-cover-extract.py
+++ album-cover-extract.py
@@ -4,10 +4,15 @@ import sys
 
 import mutagen.id3
 import mutagen.flac
+import mutagen.mp4
 
 try:
 	id3 = mutagen.id3.ID3(sys.argv[1])
 	open('/home/barosl/test.jpg', 'wb').write(id3.getall('APIC')[0].data)
 except mutagen.id3.ID3NoHeaderError:
-	flac = mutagen.flac.FLAC(sys.argv[1])
-	open('/home/barosl/test.jpg', 'wb').write(flac.pictures[0].data)
+	try:
+		flac = mutagen.flac.FLAC(sys.argv[1])
+		open('/home/barosl/test.jpg', 'wb').write(flac.pictures[0].data)
+	except mutagen.flac.FLACNoHeaderError:
+		mp4 = mutagen.mp4.MP4(sys.argv[1])
+		open('/home/barosl/test.jpg','wb').write(mp4['covr'][0])
