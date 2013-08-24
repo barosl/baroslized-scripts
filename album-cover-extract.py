847350b3b2ba31e48b3848be9aa3cf5aa8c0450c
Barosl LEE <vcs@barosl.com>
2013-08-24 14:32:36 +0900

    album-cover-extract.py: Allow extracting album covers from FLAC files

--- album-cover-extract.py
+++ album-cover-extract.py
@@ -5,11 +5,9 @@ import sys
 import mutagen.id3
 import mutagen.flac
 
-id3 = mutagen.id3.ID3(sys.argv[1])
-open('/home/barosl/test.jpg', 'wb').write(id3.getall('APIC')[0].data)
-sys.exit()
-
-flac = mutagen.flac.FLAC(sys.argv[1])
-for pic in flac.pictures:
-	print pic
-	open('/home/barosl/test.jpg', 'wb').write(pic.data)
+try:
+	id3 = mutagen.id3.ID3(sys.argv[1])
+	open('/home/barosl/test.jpg', 'wb').write(id3.getall('APIC')[0].data)
+except mutagen.id3.ID3NoHeaderError:
+	flac = mutagen.flac.FLAC(sys.argv[1])
+	open('/home/barosl/test.jpg', 'wb').write(flac.pictures[0].data)
